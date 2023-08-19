1aa20dada95bc1d299d74bd614cc6fd711119104
samar abdulaziz <sam_abdulaziz@yahoo.com>
2023-08-19 21:34:39 +0300

    adding book entity & home repo

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final double price;
+  final double rate;
+
+  BookEntity(
+    this.image,
+    this.title,
+    this.authorName,
+    this.price,
+    this.rate,
+  );
+}
