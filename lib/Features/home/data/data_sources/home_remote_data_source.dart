eed8e4ed81f125146d5c3d34e912e508567b5b8a
samar abdulaziz <sam_abdulaziz@yahoo.com>
2023-08-29 00:30:26 +0300

    cache featured books

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -1,36 +1,45 @@
 import 'package:bookly_app_tharwat/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app_tharwat/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app_tharwat/constants.dart';
 import 'package:bookly_app_tharwat/core/utils/api_service.dart';
+import 'package:bookly_app_tharwat/core/utils/functions/save_books_data.dart';
 
-abstract class HomeRemoteDataSource{
+abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks();
 
   Future<List<BookEntity>> fetchNewestBooks();
 }
-class HomeRemoteDataSourceImpl implements HomeRemoteDataSource{
+
+class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
   final ApiService apiService;
 
   HomeRemoteDataSourceImpl(this.apiService);
-  @override
-  Future<List<BookEntity>> fetchFeaturedBooks() async{
 
-    var data =await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
     List<BookEntity> books = gitBooksList(data);
+    saveBooksData(books,kFeaturedBox);
+
     return books;
   }
+
+
+
   @override
   Future<List<BookEntity>> fetchNewestBooks() async {
-    var data =await apiService.get(endPoint: 'volumes?filtering:=free&sorting=newest&q=programming');
+    var data = await apiService.get(
+        endPoint: 'volumes?filtering:=free&sorting=newest&q=programming');
     List<BookEntity> books = gitBooksList(data);
     return books;
   }
+
   List<BookEntity> gitBooksList(Map<String, dynamic> data) {
-      List<BookEntity> books=[];
-    for (var bookMap in data['items']){
+    List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
       books.add(BookModel.fromJson(bookMap));
     }
     return books;
   }
-
-
 }
---
+++ lib/core/utils/functions/save_books_data.dart
@@ -0,0 +1,7 @@
+import 'package:bookly_app_tharwat/Features/home/domain/entities/book_entity.dart';
+import 'package:hive/hive.dart';
+
+void saveBooksData(List<BookEntity> books, String boxName) {
+  var box = Hive.box(boxName);
+  box.addAll(books);
+}
